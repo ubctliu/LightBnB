f41f30c4264a4bed5fe3dad30ccddf175cb340c9
Terrence <terrence.liu@alumni.ubc.ca>
2023-09-14 11:26:19 -0700

    Implement addUser

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -53,10 +53,13 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query("INSERT INTO users (name, password, email) VALUES ($1, $2, $3) RETURNING *;", [user.name, user.password, user.email])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 /// Reservations
